6030aff496d0517c9eecb98a7cc9801a49e178fd
anju <nasreenanjuma471@gmail.com>
2023-12-07 00:36:56 +0530

    code prettier

--- src/Components/taskTypes/Kenban.js
+++ src/Components/taskTypes/Kenban.js
@@ -1,67 +1,124 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Shimmer from '../Shimmer';
-import Filters from '../Filters';
-import Modal from "./Modal"
-import TaskButton from './TaskButton';
-import Todo from './Todo';
-import Progress from './Progress';
-import Testing from './Testing';
-import Done from './Done';
-import { url, headers } from '../util/Constant';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Shimmer from "../Shimmer";
+import Filters from "../Filters";
+import Modal from "./Modal";
+import TaskButton from "./TaskButton";
+import Todo from "./Todo";
+import Progress from "./Progress";
+import Testing from "./Testing";
+import Done from "./Done";
+import { url, headers } from "../util/Constant";
 const Kenban = () => {
   const [tasks, setTasks] = useState([]);
-  const [filterState,setFilterState] = useState('');
-  const [search,setSearch] = useState('');
-  const [assigneeFilter,setAssigneeFilter] = useState('');
-  const [startDateFilter, setStartDateFilter] = useState('');
-  const [endDateFilter, setEndDateFilter] = useState('');
-const readyCount = tasks.filter(task => task.status === 'Ready' && (filterState.length>0 ?  task.priority === filterState : task.priority)  && (search.length>0 ? task.name.toLowerCase().includes(search.toLowerCase()):task.name) && ((startDateFilter ? task.startDate >= startDateFilter : true) && (endDateFilter ? task.endDate <= endDateFilter : true)) && (assigneeFilter.length>0 ? task.assignee.toLowerCase().includes(assigneeFilter.toLowerCase()):task.assignee))
-const inprogressCount = tasks.filter(task => task.status === 'In Progress' && (filterState.length>0 ?  task.priority === filterState : task.priority)  && (search.length>0 ? task.name.toLowerCase().includes(search.toLowerCase()):task.name) && ((startDateFilter ? task.startDate >= startDateFilter : true) && (endDateFilter ? task.endDate <= endDateFilter : true)) && (assigneeFilter.length>0 ? task.assignee.toLowerCase().includes(assigneeFilter.toLowerCase()):task.assignee))
-const testingCount = tasks.filter(task => task.status === 'Testing' && (filterState.length>0 ?  task.priority === filterState : task.priority)  && (search.length>0 ? task.name.toLowerCase().includes(search.toLowerCase()):task.name) && ((startDateFilter ? task.startDate >= startDateFilter : true) && (endDateFilter ? task.endDate <= endDateFilter : true)) && (assigneeFilter.length>0 ? task.assignee.toLowerCase().includes(assigneeFilter.toLowerCase()):task.assignee))
-const doneCount = tasks.filter(task => task.status === 'Done' && (filterState.length>0 ?  task.priority === filterState : task.priority)  && (search.length>0 ? task.name.toLowerCase().includes(search.toLowerCase()):task.name) && ((startDateFilter ? task.startDate >= startDateFilter : true) && (endDateFilter ? task.endDate <= endDateFilter : true)) && (assigneeFilter.length>0 ? task.assignee.toLowerCase().includes(assigneeFilter.toLowerCase()):task.assignee))
-const [newTaskName, setNewTaskName] = useState('');
-  const [newTaskAssignee, setNewTaskAssignee] = useState('');
-  const [newPriority, setNewPriority] = useState('');
-  const [newTaskState,setNewTaskState]=useState('');
-const [filteredTask,setFilteredTask] = useState('');
-  const [newDate, setNewDate]  = useState('');
+  const [filterState, setFilterState] = useState("");
+  const [search, setSearch] = useState("");
+  const [assigneeFilter, setAssigneeFilter] = useState("");
+  const [startDateFilter, setStartDateFilter] = useState("");
+  const [endDateFilter, setEndDateFilter] = useState("");
+  const readyCount = tasks.filter(
+    (task) =>
+      task.status === "Ready" &&
+      (filterState.length > 0
+        ? task.priority === filterState
+        : task.priority) &&
+      (search.length > 0
+        ? task.name.toLowerCase().includes(search.toLowerCase())
+        : task.name) &&
+      (startDateFilter ? task.startDate >= startDateFilter : true) &&
+      (endDateFilter ? task.endDate <= endDateFilter : true) &&
+      (assigneeFilter.length > 0
+        ? task.assignee.toLowerCase().includes(assigneeFilter.toLowerCase())
+        : task.assignee)
+  );
+  const inprogressCount = tasks.filter(
+    (task) =>
+      task.status === "In Progress" &&
+      (filterState.length > 0
+        ? task.priority === filterState
+        : task.priority) &&
+      (search.length > 0
+        ? task.name.toLowerCase().includes(search.toLowerCase())
+        : task.name) &&
+      (startDateFilter ? task.startDate >= startDateFilter : true) &&
+      (endDateFilter ? task.endDate <= endDateFilter : true) &&
+      (assigneeFilter.length > 0
+        ? task.assignee.toLowerCase().includes(assigneeFilter.toLowerCase())
+        : task.assignee)
+  );
+  const testingCount = tasks.filter(
+    (task) =>
+      task.status === "Testing" &&
+      (filterState.length > 0
+        ? task.priority === filterState
+        : task.priority) &&
+      (search.length > 0
+        ? task.name.toLowerCase().includes(search.toLowerCase())
+        : task.name) &&
+      (startDateFilter ? task.startDate >= startDateFilter : true) &&
+      (endDateFilter ? task.endDate <= endDateFilter : true) &&
+      (assigneeFilter.length > 0
+        ? task.assignee.toLowerCase().includes(assigneeFilter.toLowerCase())
+        : task.assignee)
+  );
+  const doneCount = tasks.filter(
+    (task) =>
+      task.status === "Done" &&
+      (filterState.length > 0
+        ? task.priority === filterState
+        : task.priority) &&
+      (search.length > 0
+        ? task.name.toLowerCase().includes(search.toLowerCase())
+        : task.name) &&
+      (startDateFilter ? task.startDate >= startDateFilter : true) &&
+      (endDateFilter ? task.endDate <= endDateFilter : true) &&
+      (assigneeFilter.length > 0
+        ? task.assignee.toLowerCase().includes(assigneeFilter.toLowerCase())
+        : task.assignee)
+  );
+  const [newTaskName, setNewTaskName] = useState("");
+  const [newTaskAssignee, setNewTaskAssignee] = useState("");
+  const [newPriority, setNewPriority] = useState("");
+  const [newTaskState, setNewTaskState] = useState("");
+  const [filteredTask, setFilteredTask] = useState("");
+  const [newDate, setNewDate] = useState("");
   const [isFormOpen, setFormOpen] = useState(false);
-const getData = () => {
-    axios.get(url, { headers })
-    .then(response => {
-      if(response.status === 200) {
-        setTasks(response.data)
-      } else {
-        console.log('Error')
-      }
-    })
-.catch(error => {
-      console.error('Error:', error.message);
-    });
-  }
-  
+  const getData = () => {
+    axios
+      .get(url, { headers })
+      .then((response) => {
+        if (response.status === 200) {
+          setTasks(response.data);
+        } else {
+          console.log("Error");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+  };
+
   useEffect(() => {
-    getData()
-  },[])
-//Modal PopUP open close
+    getData();
+  }, []);
+  //Modal PopUP open close
   const openForm = () => {
     setFormOpen(true);
   };
   const closeTask = () => {
     setFormOpen(false);
   };
-// Drag and Drop
+  // Drag and Drop
   const handleDragStart = (e, taskId) => {
-    e.dataTransfer.setData('text/plain', taskId.toString());
+    e.dataTransfer.setData("text/plain", taskId.toString());
   };
   const handleDragOver = (e) => {
     e.preventDefault();
   };
-const handleDrop = (e, targetStatus) => {
+  const handleDrop = (e, targetStatus) => {
     e.preventDefault();
-    const taskId = parseInt(e.dataTransfer.getData('text/plain'), 10);
-    const updatedTasks = tasks.map(task => {
+    const taskId = parseInt(e.dataTransfer.getData("text/plain"), 10);
+    const updatedTasks = tasks.map((task) => {
       if (task.id === taskId) {
         return { ...task, status: targetStatus };
       }
@@ -76,140 +133,169 @@ const handleDrop = (e, targetStatus) => {
       name: newTaskName,
       assignee: newTaskAssignee,
       startDate: newDate,
-      priority: newPriority, 
+      priority: newPriority,
       status: newTaskState,
-      effortSpent: 0, 
+      effortSpent: 0,
     };
-setTasks([...tasks, newTask]);
-    setNewTaskName('');
-    setNewPriority('');
-    setNewDate('');
-    setNewTaskState('');
-    setNewTaskAssignee('');
+    setTasks([...tasks, newTask]);
+    setNewTaskName("");
+    setNewPriority("");
+    setNewDate("");
+    setNewTaskState("");
+    setNewTaskAssignee("");
     setFormOpen(false);
   };
-//Filters
+  //Filters
   const filterChange = (e) => {
     setFilterState(e.target.value);
-    const updatedTasks = tasks.filter(task => 
-      task.priority === e.target.value
-     );
-     setFilteredTask(updatedTasks)
-  }
-const searchChange = (e) => {
-    setSearch(e.target.value)
-  }
+    const updatedTasks = tasks.filter(
+      (task) => task.priority === e.target.value
+    );
+    setFilteredTask(updatedTasks);
+  };
+  const searchChange = (e) => {
+    setSearch(e.target.value);
+  };
   const endDateChange = (e) => {
-    setEndDateFilter(e.target.value)
-  }
+    setEndDateFilter(e.target.value);
+  };
   const startDateChange = (e) => {
-    setStartDateFilter(e.target.value)
-  }
-const assigneeFilterChange = (e) => {
-    setAssigneeFilter(e.target.value)
-  }
+    setStartDateFilter(e.target.value);
+  };
+  const assigneeFilterChange = (e) => {
+    setAssigneeFilter(e.target.value);
+  };
 
   //modal popup
   const modalNameChange = (e) => {
-    setNewTaskName(e.target.value)
-  }
-const modalAssigneeNameChange = (e) => {
-    setNewTaskAssignee(e.target.value)
-  }
+    setNewTaskName(e.target.value);
+  };
+  const modalAssigneeNameChange = (e) => {
+    setNewTaskAssignee(e.target.value);
+  };
   const modalPriorityChange = (e) => {
-    setNewPriority(e.target.value)
-  }
+    setNewPriority(e.target.value);
+  };
   const modalTaskStateChange = (e) => {
-    setNewTaskState(e.target.value)
-  }
-return tasks.length === 0 ? (<Shimmer />) : (
+    setNewTaskState(e.target.value);
+  };
+  return tasks.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div>
-      <div className='pt-[10%]'>
-        <Filters 
-          filterState={filterState} 
-          filterChange={filterChange} 
+      <div className="pt-[10%]">
+        <Filters
+          filterState={filterState}
+          filterChange={filterChange}
           search={search}
-searchChange={searchChange} 
-          startDateFilter={startDateFilter} 
-          startDateChange={startDateChange} 
-          endDateFilter={endDateFilter} 
-          endDateChange={endDateChange} 
-          assigneeFilterChange={assigneeFilterChange} 
-          assigneeFilter={assigneeFilter}/>
+          searchChange={searchChange}
+          startDateFilter={startDateFilter}
+          startDateChange={startDateChange}
+          endDateFilter={endDateFilter}
+          endDateChange={endDateChange}
+          assigneeFilterChange={assigneeFilterChange}
+          assigneeFilter={assigneeFilter}
+        />
       </div>
       <div className="flex ml-[8%] pt-[3%] gap-[1%]">
-        <div className="column"> 
-          <h2 className='font-bold border-b-2 pb-[10%] mb-[10%]'>BACKLOG(0)</h2>
-          <div onClick={openForm}><TaskButton /></div>
-        </div>
-<div className="column" onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e, 'Ready')}>
-          <Todo 
-            readyCount={readyCount} 
-            tasks={tasks} 
-            filterState={filterState} 
-            search={search}
-startDateFilter={startDateFilter} 
-            endDateFilter={endDateFilter} 
-            assigneeFilter={assigneeFilter} 
-            handleDragStart={handleDragStart}/>
-          <div onClick={openForm}><TaskButton /></div>
-        </div>
-<div className="column" onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e, 'In Progres')}>
-          <Progress 
-            inprogressCount={inprogressCount} 
-            tasks={tasks} 
-            filterState={filterState} 
-            search={search}
-startDateFilter={startDateFilter} 
-            endDateFilter={endDateFilter} 
-            assigneeFilter={assigneeFilter} 
-            handleDragStart={handleDragStart}/>
-          <div onClick={openForm}> 
+        <div className="column">
+          <h2 className="font-bold border-b-2 pb-[10%] mb-[10%]">BACKLOG(0)</h2>
+          <div onClick={openForm}>
             <TaskButton />
           </div>
         </div>
-<div className="column" onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e, 'Testing')}>
-          <Testing 
-            testingCount={testingCount} 
-            tasks={tasks} 
-            filterState={filterState} 
+        <div
+          className="column"
+          onDragOver={(e) => handleDragOver(e)}
+          onDrop={(e) => handleDrop(e, "Ready")}
+        >
+          <Todo
+            readyCount={readyCount}
+            tasks={tasks}
+            filterState={filterState}
             search={search}
-startDateFilter={startDateFilter} 
-            endDateFilter={endDateFilter} 
-            assigneeFilter={assigneeFilter} 
-            handleDragStart={handleDragStart}/>
-          <div onClick={openForm}> 
+            startDateFilter={startDateFilter}
+            endDateFilter={endDateFilter}
+            assigneeFilter={assigneeFilter}
+            handleDragStart={handleDragStart}
+          />
+          <div onClick={openForm}>
             <TaskButton />
           </div>
         </div>
-<div className="column" onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e, 'Done')}>
-          <Done 
-            doneCount={doneCount} 
-            tasks={tasks} 
-            filterState={filterState} 
+        <div
+          className="column"
+          onDragOver={(e) => handleDragOver(e)}
+          onDrop={(e) => handleDrop(e, "In Progres")}
+        >
+          <Progress
+            inprogressCount={inprogressCount}
+            tasks={tasks}
+            filterState={filterState}
             search={search}
-startDateFilter={startDateFilter} 
-            endDateFilter={endDateFilter} 
-            assigneeFilter={assigneeFilter} 
-            handleDragStart={handleDragStart}/>
-          <div onClick={openForm}> 
+            startDateFilter={startDateFilter}
+            endDateFilter={endDateFilter}
+            assigneeFilter={assigneeFilter}
+            handleDragStart={handleDragStart}
+          />
+          <div onClick={openForm}>
+            <TaskButton />
+          </div>
+        </div>
+        <div
+          className="column"
+          onDragOver={(e) => handleDragOver(e)}
+          onDrop={(e) => handleDrop(e, "Testing")}
+        >
+          <Testing
+            testingCount={testingCount}
+            tasks={tasks}
+            filterState={filterState}
+            search={search}
+            startDateFilter={startDateFilter}
+            endDateFilter={endDateFilter}
+            assigneeFilter={assigneeFilter}
+            handleDragStart={handleDragStart}
+          />
+          <div onClick={openForm}>
+            <TaskButton />
+          </div>
+        </div>
+        <div
+          className="column"
+          onDragOver={(e) => handleDragOver(e)}
+          onDrop={(e) => handleDrop(e, "Done")}
+        >
+          <Done
+            doneCount={doneCount}
+            tasks={tasks}
+            filterState={filterState}
+            search={search}
+            startDateFilter={startDateFilter}
+            endDateFilter={endDateFilter}
+            assigneeFilter={assigneeFilter}
+            handleDragStart={handleDragStart}
+          />
+          <div onClick={openForm}>
             <TaskButton />
           </div>
         </div>
         {isFormOpen && (
           <div className="fixed w-[100%] h-[100%] text-center justify-center left-0 top-0 border-[2px] border-black create-task">
-            <Modal 
+            <Modal
               newTaskName={newTaskName}
-modalNameChange={modalNameChange} 
-              newTaskAssignee={newTaskAssignee} 
-              modalAssigneeNameChange={modalAssigneeNameChange} 
-              newPriority={newPriority} 
+              modalNameChange={modalNameChange}
+              newTaskAssignee={newTaskAssignee}
+              modalAssigneeNameChange={modalAssigneeNameChange}
+              newPriority={newPriority}
               modalPriorityChange={modalPriorityChange}
-newTaskState={newTaskState} 
-              modalTaskStateChange={modalTaskStateChange} 
-              closeTask={closeTask} 
-              handleCreateTask={handleCreateTask}/>
-          </div>)}
+              newTaskState={newTaskState}
+              modalTaskStateChange={modalTaskStateChange}
+              closeTask={closeTask}
+              handleCreateTask={handleCreateTask}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/Components/taskTypes/Modal.js
+++ src/Components/taskTypes/Modal.js
@@ -1,46 +1,95 @@
-export default function Modal({newTaskName,modalNameChange,newTaskAssignee,modalAssigneeNameChange,newPriority,modalPriorityChange,newTaskState,modalTaskStateChange,closeTask,handleCreateTask}) {
-    return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg mt-[15%] ml-[35%] bg-neutral-400" >
-            <div className="pb-[10%]">
-                <div className="font-bold text-xl mb-2">Create New Task</div>
-                    <table>
-    <tr>
-                            <td><label className='p-5'>Name</label></td>
-                            <td><input className="border-[1px]  mb-2 border-black rounded p-1 bg-neutral-300" type="text" value={newTaskName} onChange={modalNameChange} /></td>
-                        </tr>
-                        <tr>
-                            <td><label className='p-5'>Assignee</label></td>
-                            <td><input className="border-[1px]  mb-2 border-black rounded p-1 bg-neutral-300" type="text"
-    value={newTaskAssignee} onChange={modalAssigneeNameChange} /></td>
-                        </tr>
-                        <tr>
-                            <td><label className='p-5'>Priority</label></td>
-                            <td>
-    <select className="border-[1px]  mb-2 border-black rounded p-1 w-[100%] bg-neutral-300" value={newPriority} onChange={modalPriorityChange}>
-    <option value="">Select</option>
-                                    <option value="High">High</option>
-                                    <option value="Medium">Medium</option>
-                                    <option value="Low">Low</option>
-                                </select>
-                            </td>
-    </tr>
-                        <tr>
-                            <td><label className='p-5'>State:</label></td>
-                            <td>
-                               <select className="border-[1px] bg-neutral-300 mb-2 border-black rounded p-1 w-[100%]" value={newTaskState} onChange={modalTaskStateChange}>
-                                    
-                               <option value="">Select</option>
-                                    <option value="Ready">Ready</option>
-                                    <option value="In Progress">In Progress</option>
-                                    <option value="Testing">Testing</option>
-                                    <option value="Done">Done</option>
-                                </select>
-                            </td>
-                        </tr>
-                    </table>
-    <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded" onClick={handleCreateTask}>Create Task</button>
-                    <button className="bg-gray-500 text-white font-bold py-2 px-4 rounded ml-[10%]" onClick={closeTask}>Close</button>
+export default function Modal({
+  newTaskName,
+  modalNameChange,
+  newTaskAssignee,
+  modalAssigneeNameChange,
+  newPriority,
+  modalPriorityChange,
+  newTaskState,
+  modalTaskStateChange,
+  closeTask,
+  handleCreateTask,
+}) {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg mt-[15%] ml-[35%] bg-neutral-400">
+      <div className="pb-[10%]">
+        <div className="font-bold text-xl mb-2">Create New Task</div>
+        <table>
+          <tr>
+            <td>
+              <label className="p-5">Name</label>
+            </td>
+            <td>
+              <input
+                className="border-[1px]  mb-2 border-black rounded p-1 bg-neutral-300"
+                type="text"
+                value={newTaskName}
+                onChange={modalNameChange}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label className="p-5">Assignee</label>
+            </td>
+            <td>
+              <input
+                className="border-[1px]  mb-2 border-black rounded p-1 bg-neutral-300"
+                type="text"
+                value={newTaskAssignee}
+                onChange={modalAssigneeNameChange}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label className="p-5">Priority</label>
+            </td>
+            <td>
+              <select
+                className="border-[1px]  mb-2 border-black rounded p-1 w-[100%] bg-neutral-300"
+                value={newPriority}
+                onChange={modalPriorityChange}
+              >
+                <option value="">Select</option>
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label className="p-5">State:</label>
+            </td>
+            <td>
+              <select
+                className="border-[1px] bg-neutral-300 mb-2 border-black rounded p-1 w-[100%]"
+                value={newTaskState}
+                onChange={modalTaskStateChange}
+              >
+                <option value="">Select</option>
+                <option value="Ready">Ready</option>
+                <option value="In Progress">In Progress</option>
+                <option value="Testing">Testing</option>
+                <option value="Done">Done</option>
+              </select>
+            </td>
+          </tr>
+        </table>
+        <button
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+          onClick={handleCreateTask}
+        >
+          Create Task
+        </button>
+        <button
+          className="bg-gray-500 text-white font-bold py-2 px-4 rounded ml-[10%]"
+          onClick={closeTask}
+        >
+          Close
+        </button>
+      </div>
     </div>
-            </div>
-        )
-    }
+  );
+}
